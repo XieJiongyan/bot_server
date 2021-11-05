a9c1736e1704b8437a212ae067815672e22ee141
xiejiongyan <xjy-master@sjtu.edu.cn>
2021-11-05 23:49:24 +0800

    J.0.0.6 success tcp

--- bot_server/tcp_server.go
+++ bot_server/tcp_server.go
@@ -18,18 +18,19 @@ func coonHandler(c net.Conn) {
 			c.Close()
 			break
 		}
-		c.Write([]byte("I see you kotlin"))
+		c.Write([]byte("I see you kotlin\n"))
 	}
 }
 
 func Start_tcp_server() {
-	tcp_server, err := net.Listen("tcp", "localhost:8001")
+	tcp_server, err := net.Listen("tcp", ":8100")
 	if err != nil {
 		log.Panic("having wrong to open tcp")
 	}
 	fmt.Println("Successfully open tcp_server")
 	for {
 		coon, err := tcp_server.Accept()
+		fmt.Println("Accept tcp server")
 		if err != nil {
 			fmt.Println("连接出错")
 			fmt.Println(err)
